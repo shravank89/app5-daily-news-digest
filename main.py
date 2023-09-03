d50232dbfc637aa5fb0cd98d9bb7926b622bad2e
Shravan <newshravank25@gmail.com>
2023-09-03 23:05:23 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests as re
+from send_email import send_email
+
+
+API_KEY = "d7bf198e298449c287b65b182fcd58dc"
+
+url = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}"
+
+# Make Requests
+request = re.get(url)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+data = ""
+for article in content["articles"]:
+    if article["title"] is not None and article["url"] is not None and article["description"] is not None :
+        data += article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+
+
+send_email(data.encode('utf-8'))
+
+
